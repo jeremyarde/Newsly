1223964cb99956e65829698d3761725c5d141d39
JEREMY-LENOVO\Jeremy <j_arde@live.ca>
2018-10-15 20:09:14 -0500

    More setup

---
+++ src/Text_Utilities/Text_Summarization.py
@@ -0,0 +1,12 @@
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
+def things():
+    stemmer = Stemmer()
+    summarizer = Summarizer(stemmer)
+
