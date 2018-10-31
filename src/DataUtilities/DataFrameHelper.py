cedde898b65609ae42986cac099f9ac3c17a7638
JEREMY-LENOVO\Jeremy <j_arde@live.ca>
2018-10-30 20:39:17 -0500

    main method

--- src/DataUtilities/DataFrameHelper.py
+++ src/DataUtilities/DataFrameHelper.py
@@ -1,4 +1,10 @@
+import os
+
 import pandas
 
-def csv_to_dataframe(path_to_csv):
-    pandas.read_csv()
+def csv_to_dataframe(path_to_csv: str=''):
+    t = os.getcwd()
+    # pandas.read_csv("src/Data/crawled_data.csv")
+
+
+csv_to_dataframe()
