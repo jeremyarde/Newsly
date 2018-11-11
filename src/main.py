4432008877e171ace78bcfb0a16df64ab45ee794
JEREMY-LENOVO\Jeremy <j_arde@live.ca>
2018-11-11 11:43:58 -0600

    filtering out data better

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from configparser import ConfigParser
 import numpy as np
 from numpy import unicode
 from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import make_scorer, fbeta_score, accuracy_score
 from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
 from sumy.nlp.stemmers import Stemmer
 from sumy.nlp.tokenizers import Tokenizer
 
@@ -15,7 +17,7 @@ from src.Models.TestModel import ModelType
 from src.Summarizers.BaseSummarizer import BaseSummarizer
 from src.Summarizers.SumySummarizer import SumySummarizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 
 config = ConfigParser()
 config.read('../config.ini')
@@ -46,11 +48,15 @@ df = DataHelper.read_csv(config['PATHS']['DataCsv'])
 df = df.dropna()  # drop all rows with nan
 biases_unique = df['BIAS'].astype('U').unique()
 classes = df['CLASS'].unique()
+
+bias_classes = ['right, Least-biased', 'left', 'Left-center', 'Right-center']
+# filter out the undesired labels:
+df = df[df['BIAS'].isin(bias_classes)]
+
 sources = df['SOURCE']
 titles = df['TITLE']
 
-model = linear_model.LogisticRegression()
-vectorizer = TfidfVectorizer()
+
 
 # replace newlines ( replace with tokenization later...
 text_data = df['TITLE'].str.replace('\n', '')
@@ -58,30 +64,42 @@ train_labels = df['BIAS'].str.replace('\n', '')
 
 # text_list_unicode = [unicode(s, 'utf-8') for s in text_list]
 text_data = text_data.values.astype('U')
+vectorizer = TfidfVectorizer()
 train_data = vectorizer.fit_transform(text_data)
 train_labels = train_labels.astype('U')
 
 x_train, x_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.3, random_state=1)
 
+# # Grid search
+# svc = SVC(gamma='scale')
+# parameters = {'kernel':('linear', 'rbf'), 'C': [1, 10]}
+# print('Starting Gridsearch...')
+# clf = GridSearchCV(svc, parameters, cv=5, verbose=True, n_jobs=5)
+# clf.fit(x_train, y_train)
+# sorted(clf.cv_results_.keys())
+# clf.score(x_test, y_test)
 
-svc = SVC(gamma='scale')
-parameters = {'kernel':('linear', 'rbf'), 'C': [1, 10]}
-print('Starting Gridsearch...')
-clf = GridSearchCV(svc, parameters, cv=5, verbose=True, n_jobs=5)
-clf.fit(x_train, y_train)
-sorted(clf.cv_results_.keys())
-
-# svc = LinearSVC()
-# svc.fit(X=x_train, y=y_train)
-# print(svc.coef_)
-# mean = svc.score(x_test, y_test)
-# mean_string = f"Mean accuracy: {mean}"
-# print(mean_string)
+print("Linear SVC")
+svc = LinearSVC(verbose=True)
+svc.fit(X=x_train, y=y_train)
+print(svc.coef_)
+mean = svc.score(x_test, y_test)
+mean_string = f"Mean accuracy: {mean}"
+print(mean_string)
 
 
-# grid = GridSearchCV(LinearSVC(), scoring=make_scorer(accuracy_score), param_grid={'C': [1, 10]})
-# grid = grid.fit(x_train, y_train)
+# Decision Tree
+print("Decision Tree")
+clf = DecisionTreeClassifier(max_depth=None, min_samples_split=2, random_state=0)
+scores = cross_val_score(clf, x_train, y_train, cv=5)
+print(scores.mean())
 
-# model.fit(x_train, y_train)
+
+# Random Forrest
+print("Random Forest")
+clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
+scores = cross_val_score(clf, x_train, y_train, cv=5)
+print(scores.mean())
+
 
 print("")
