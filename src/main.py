02dbcc79abed3317bed1f9654628b828da8ff11b
JEREMY-LENOVO\Jeremy <j_arde@live.ca>
2018-12-08 13:09:44 -0600

    - best params highlighted - add model pickling

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 from configparser import ConfigParser
 
+import dill
 import keras
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
@@ -47,22 +48,47 @@ model = Keras(max_words=num_words, num_classes=len(labels))
 param_grid = dict(
     epochs=[30],
     batch_size=[500],
-    dense_layers=[128],
-    optimizer=['adam',
-               'sgd',
-               'nadam?',
-               'adad?elta'
-               ],
-    activation=['relu',
-                'softmax',
-                'elu',
-                'tanh',
-                'linear'
-                ],
-    dropout=[0.5]
+    dense_layers=[
+        # 30,
+        # 60,
+        # 128,
+        # 150,
+        # 200,
+        250,
+        350,
+        500,
+        1000,
+        2000
+    ],
+    optimizer=[
+        # 'adam',
+        # 'sgd',
+        # 'nadam?',
+        'adad?elta'
+    ],
+    activation=[
+        # 'relu',
+        # 'softmax',
+        'elu',
+        # 'tanh',
+        # 'linear'
+    ],
+    dropout=[
+        # 0.0,
+        # 0.5,
+        # 0.75,
+        0.90
+    ]
 )
 
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
 grid_result = grid.fit(x_train, y_train)
 
+best_model = Keras(**grid_result.best_params_)
+best_model.fit(x_train, y_train)
+best_model.model.evaluate(x_test, y_test)
+
+pickled_model = best_model
+with open("model_test.dill", "wb") as f:
+    dill.dump((best_model), f)
 print("Done")
