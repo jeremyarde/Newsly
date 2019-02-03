b58d6fe3b77763be587963ce51c1af5f22a7a62c
JEREMY-LENOVO\Jeremy <jere.arde@gmail.com>
2019-02-03 00:02:23 -0600

    update analysing logic

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import os
+import pandas
 from configparser import ConfigParser
+
+from nltk import RegexpTokenizer
+
 from src import TweetGrabber, ModelBuilder
 
+from collections import Counter
 from keras.preprocessing.text import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 
-stopwords = stopwords.words('english')
+# stopwords = stopwords.words('english')
 
 config = ConfigParser()
 config.read('../config.ini')
@@ -15,6 +20,8 @@ config.read('../config.ini')
 
 entities = ['indiginous', 'development', 'human rights', 'crime', 'budget', 'housing']
 people = ['Vivs4PDW', 'JaniceLukes', 'kevinkleinwpg', 'cindygilroy', 'B_MayesSTV']
+df = pandas.DataFrame()
+tok = TfidfVectorizer(lowercase=True, analyzer='word', stop_words='english')
 
 people_tweets = []
 for person in people:
@@ -26,12 +33,19 @@ tweet_text_list = []
 for person_tweets in people_tweets:
     person_tweet_text = []
     for tweet in person_tweets:
-        person_tweet_text.append(tweet.get('tweet_text'))
-    tweet_text_list.append(person_tweet_text)
+        person_tweet_text.append(tweet.get('tweet_text')[0])
+
+    # string_list = ["".join(x) for x in person_tweets]
+
+    tokenizer = RegexpTokenizer(r'\w+')
+    zen_no_punc = tokenizer.tokenize(' '.join(string_list))
+    word_count_dict = Counter(w.title() for w in zen_no_punc if w.lower() not in stopwords.words())
+    common = word_count_dict.most_common()
+
+    word_freq_df = pandas.DataFrame(common)
+
+    df = pandas.concat([df, word_freq_df])
 
 
-tok = TfidfVectorizer(lowercase=True, analyzer='word', stop_words='english')
-for person_tweets in tweet_text_list:
-    tokenized = tok.fit_transform("".join(person_tweets))
-
-print('Done')
+# remove_list = ['Https', 'Co', 'Rt', 'Amp']
+df.to_csv("word_counts.csv")
